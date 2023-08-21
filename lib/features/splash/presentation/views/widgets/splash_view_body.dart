2282b937d846a509b6f942f9f83439022b55c70e
saci zakaria <sacideveloppeur@gmail.com>
2023-08-21 08:21:26 +0200

    Constructors in '@immutable' classes should be declared as 'const'. Try adding 'const' to the constructor declaration.

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class SplashviewBody extends StatelessWidget {
   // const SplashviewBody({super.key});
-  const SplashviewBody({Key? key}) : super(key: key);
+  SplashviewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
