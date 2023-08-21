4f3003b616dc09ce3e54adbc12ff221dc051c2de
saci zakaria <sacideveloppeur@gmail.com>
2023-08-21 08:20:16 +0200

    const SplashviewBody({Key? key}) : super(key: key);

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class SplashviewBody extends StatelessWidget {
-  const SplashviewBody({super.key});
+  // const SplashviewBody({super.key});
+  const SplashviewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return Column();
   }
 }
