437fe4db43925648a9a9957b05d82f8196cd13fe
saci zakaria <sacideveloppeur@gmail.com>
2023-08-29 11:35:09 +0200

    lass CustomAppBar extends StatelessWidget

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -10,3 +10,12 @@ class HomeViewBody extends StatelessWidget {
     );
   }
 }
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
