2dba5f5b455c67deb3699dac93260d09bc44fa13
saci zakaria <sacideveloppeur@gmail.com>
2023-08-29 11:35:55 +0200

    CustomAppBar(),

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -6,7 +6,9 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [],
+      children: [
+        CustomAppBar(),
+      ],
     );
   }
 }
