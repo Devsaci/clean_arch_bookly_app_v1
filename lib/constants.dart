65460fa6e87e20877de3ae5d9747de15f641c142
saci zakaria <sacideveloppeur@gmail.com>
2023-09-05 19:21:24 +0200

    const kGtSectraFine = 'GT Sectra Fine';

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color.fromARGB(255, 29, 15, 74);
 const kTranstionDuration = Duration(milliseconds: 50);
+const kGtSectraFine = 'GT Sectra Fine';
