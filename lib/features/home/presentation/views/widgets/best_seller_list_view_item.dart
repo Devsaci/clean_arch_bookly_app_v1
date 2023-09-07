ef0a4aeca6abd1d9d29edb9405dc060c708cdd59
saci zakaria <sacideveloppeur@gmail.com>
2023-09-07 10:39:07 +0200

    move class BestSellerListView

---
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -0,0 +1,54 @@
+import 'package:clean_arch_bookly_app_v1/constants.dart';
+import 'package:clean_arch_bookly_app_v1/core/uttlls/assets.dart';
+import 'package:clean_arch_bookly_app_v1/core/uttlls/styles.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.8 / 4,
+            child: Container(
+              //height: MediaQuery.of(context).size.height * .25,
+              // width: 100,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(60),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    AssetsData.testImage,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 30),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: Text(
+                  'Harry Potter and the Goblet of Fire',
+                  style: Styles.textStyle16.copyWith(fontFamily: kGtSectraFine),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              const SizedBox(height: 3),
+              const Text('J.K. Rowling', style: Styles.textStyle14),
+              const SizedBox(height: 3),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:clean_arch_bookly_app_v1/constants.dart';
 import 'package:clean_arch_bookly_app_v1/core/uttlls/assets.dart';
 import 'package:clean_arch_bookly_app_v1/core/uttlls/styles.dart';
+import 'package:clean_arch_bookly_app_v1/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
@@ -31,55 +32,7 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.8 / 4,
-            child: Container(
-              //height: MediaQuery.of(context).size.height * .25,
-              // width: 100,
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(60),
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    AssetsData.testImage,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(width: 30),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: Text(
-                  'Harry Potter and the Goblet of Fire',
-                  style: Styles.textStyle16.copyWith(fontFamily: kGtSectraFine),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-              const SizedBox(height: 3),
-              const Text('J.K. Rowling', style: Styles.textStyle14),
-              const SizedBox(height: 3),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
 
 // class FeaturedBooksListView extends StatelessWidget {
 //   const FeaturedBooksListView({super.key});
