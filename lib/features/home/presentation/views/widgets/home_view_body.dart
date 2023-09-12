a9ae752c8b30fb5b8e1befe230649949b00adcf6
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-12 15:00:29 +0200

    class BestSellerListView extends StatelessWidget

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -32,23 +32,11 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
 
-
-// class FeaturedBooksListView extends StatelessWidget {
-//   const FeaturedBooksListView({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SizedBox(
-//       height: MediaQuery.of(context).size.height * .3,
-//       child: ListView.builder(
-//         itemCount: 5,
-//         scrollDirection: Axis.horizontal,
-//         itemBuilder: (context, index) => const Padding(
-//           padding: EdgeInsets.symmetric(horizontal: 8),
-//           child: FeaturedListViewItem(),
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
