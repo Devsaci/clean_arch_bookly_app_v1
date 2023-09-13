a80217eb5d7176b1fa46c8d1b3dbaae06ef04ec1
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-13 18:57:51 +0200

    Removed import

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -2,7 +2,6 @@ import 'package:clean_arch_bookly_app_v1/constants.dart';
 
 import 'package:clean_arch_bookly_app_v1/core/uttlls/styles.dart';
 import 'package:clean_arch_bookly_app_v1/features/home/presentation/views/widgets/best_seller_list_view.dart';
-import 'package:clean_arch_bookly_app_v1/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
