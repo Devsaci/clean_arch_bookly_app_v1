613fbf8608ac947f1966c5ad66133a76a967de9b
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-14 09:44:28 +0200

    final GoRouter _router = GoRouter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:clean_arch_bookly_app_v1/constants.dart';
+import 'package:clean_arch_bookly_app_v1/features/splash/presentation/views/splash_view.dart';
 //import 'package:clean_arch_bookly_app_v1/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 //import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -8,12 +10,33 @@ void main() {
   runApp(const Bookly());
 }
 
+/// The route configuration.
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SplashView();
+      },
+      // routes: <RouteBase>[
+      //   GoRoute(
+      //     path: 'details',
+      //     builder: (BuildContext context, GoRouterState state) {
+      //       return const DetailsScreen();
+      //     },
+      //   ),
+      // ],
+    ),
+  ],
+);
+
 class Bookly extends StatelessWidget {
   const Bookly({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      routerConfig: _router,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
         textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
