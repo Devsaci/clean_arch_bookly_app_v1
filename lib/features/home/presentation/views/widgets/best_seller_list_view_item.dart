a01f782763e0b5c3d65caef348d47215700e50fe
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-18 10:06:35 +0200

    GestureDetector

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -9,58 +9,61 @@ class BestSellerListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.8 / 4,
-            child: Container(
-              //height: MediaQuery.of(context).size.height * .25,
-              // width: 100,
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(60),
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    AssetsData.testImage,
+    return GestureDetector(
+      onTap: () {},
+      child: SizedBox(
+        height: 125,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.8 / 4,
+              child: Container(
+                //height: MediaQuery.of(context).size.height * .25,
+                // width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(60),
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      AssetsData.testImage,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(width: 30),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .9,
-                  child: Text(
-                    'Harry Potter and the Goblet of Fire',
-                    style:
-                        Styles.textStyle16.copyWith(fontFamily: kGtSectraFine),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
+            const SizedBox(width: 30),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .9,
+                    child: Text(
+                      'Harry Potter and the Goblet of Fire',
+                      style: Styles.textStyle16
+                          .copyWith(fontFamily: kGtSectraFine),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 3),
-                const Text('J.K. Rowling', style: Styles.textStyle14),
-                const SizedBox(height: 3),
-                Row(
-                  children: [
-                    Text("19.99 eu",
-                        style: Styles.textStyle14
-                            .copyWith(fontWeight: FontWeight.bold)),
-                    const Spacer(),
-                    const BookRating(),
-                  ],
-                ),
-              ],
-            ),
-          )
-        ],
+                  const SizedBox(height: 3),
+                  const Text('J.K. Rowling', style: Styles.textStyle14),
+                  const SizedBox(height: 3),
+                  Row(
+                    children: [
+                      Text("19.99 eu",
+                          style: Styles.textStyle14
+                              .copyWith(fontWeight: FontWeight.bold)),
+                      const Spacer(),
+                      const BookRating(),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
