0261df16a1907976f7a9636b7e9149d7242bd9b9
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-18 10:40:33 +0200

    MainAxisAlignment.spaceBetween

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -19,6 +19,7 @@ class CustomBookDetailsAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         IconButton(onPressed: () {}, icon: const Icon(Icons.close)),
         IconButton(
