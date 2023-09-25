04b39e8cd93ca2dce0378c1da53bcbe25409c2c9
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-25 14:16:28 +0200

    Text  'The Jungle Book'

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:clean_arch_bookly_app_v1/features/home/presentation/views/widgets/custom_book_ithem.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/uttlls/styles.dart';
 import 'custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -19,6 +20,10 @@ class BookDetailsViewBody extends StatelessWidget {
             child: const CustumBookImage(),
           ),
           const SizedBox(height: 43),
+          Text(
+            'The Jungle Book',
+            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
         ],
       ),
     );
