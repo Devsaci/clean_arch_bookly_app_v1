6d5e7d9bd59b3c9be9720f7f61fa7ee7472c37c7
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-26 15:10:24 +0200

    style: Styles.textStyle14.copyWith

--- lib/core/widgers/custom_button.dart
+++ lib/core/widgers/custom_button.dart
@@ -1,11 +1,19 @@
+import 'package:clean_arch_bookly_app_v1/core/uttlls/styles.dart';
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
   @override
   Widget build(BuildContext context) {
     return TextButton(
       onPressed: () {},
-      child: const Text('19.99€'),
+      child: Text(
+        '19.99€',
+        style: Styles.textStyle14.copyWith(
+          color: Colors.black,
+        ),
+      ),
     );
   }
 }
