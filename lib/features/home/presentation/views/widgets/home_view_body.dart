dcbcbaa8e9d2cb63c4c61e7f43d605c0156926f9
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-28 10:40:57 +0200

    removed import 'featured_book_list_view.dart';

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -5,7 +5,8 @@ import 'package:clean_arch_bookly_app_v1/features/home/presentation/views/widget
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
-import 'featured_book_list_view.dart';
+
+import 'featured_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
