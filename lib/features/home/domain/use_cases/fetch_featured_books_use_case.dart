aef87d00b4d729136e2a174f4525a7023e8f70d2
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-10-02 14:16:35 +0200

    Future<Either<Failure, List<BookEntity>>>

--- lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,10 +1,16 @@
+import 'package:clean_arch_bookly_app_v1/core/errors/failure.dart';
+
+import 'package:clean_arch_bookly_app_v1/features/home/domain/entities/book_entitiy.dart';
+
+import 'package:dartz/dartz.dart';
+
 import '../repos/home_repo.dart';
 
 class FetchFeaturedBooksUseCase {
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase(this.homeRepo);
-  fetchFeaturedBooks() {
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
     return homeRepo.fetchFeaturedBooks();
   }
 }
