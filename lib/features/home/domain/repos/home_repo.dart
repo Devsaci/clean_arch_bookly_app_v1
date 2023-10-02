7c95f3e59b7c1738f8d1077c2b0b905aa7a152bc
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-10-02 10:22:38 +0200

    Future<BookEntity> fetchFeaturedBooks();

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,1 +1,5 @@
-abstract class HomeRepo {}
+import 'package:clean_arch_bookly_app_v1/features/home/domain/entities/book_entitiy.dart';
+
+abstract class HomeRepo {
+  Future<BookEntity> fetchFeaturedBooks();
+}
