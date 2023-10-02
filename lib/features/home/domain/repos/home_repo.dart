532c64f45a2771ddf3c2dfe6cc2fcc6c5d06247d
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-10-02 10:24:57 +0200

    <List<BookEntity>>

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,5 +1,5 @@
 import 'package:clean_arch_bookly_app_v1/features/home/domain/entities/book_entitiy.dart';
 
 abstract class HomeRepo {
-  Future<BookEntity> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchFeaturedBooks();
 }
