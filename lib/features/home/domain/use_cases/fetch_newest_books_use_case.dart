b441018266107bd8d65ada9e0e3fb1a0bf71443a
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-10-02 15:08:06 +0200

    extends UseCase<List<BookEntity>, NoParam>

--- lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -1,1 +1,14 @@
-class FetchNewestdBooksUseCase {}
+import 'package:clean_arch_bookly_app_v1/core/errors/failure.dart';
+
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/use_case/use_case.dart';
+import '../entities/book_entitiy.dart';
+
+class FetchNewestdBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
+  @override
+  Future<Either<Failure, List<BookEntity>>> call() {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+}
