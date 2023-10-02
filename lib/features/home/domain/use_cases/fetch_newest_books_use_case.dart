8bb781e2d74bcf25d2b0448830b840cc8a9325f4
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-10-02 15:12:31 +0200

    return await homeRepo.fetchNewsetBooks();;

--- lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -1,4 +1,5 @@
 import 'package:clean_arch_bookly_app_v1/core/errors/failure.dart';
+import 'package:clean_arch_bookly_app_v1/features/home/domain/repos/home_repo.dart';
 
 import 'package:dartz/dartz.dart';
 
@@ -6,9 +7,11 @@ import '../../../../core/use_case/use_case.dart';
 import '../entities/book_entitiy.dart';
 
 class FetchNewestdBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
+  final HomeRepo homeRepo;
+
+  FetchNewestdBooksUseCase(this.homeRepo);
   @override
-  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+    return await homeRepo.fetchNewsetBooks();
   }
 }
