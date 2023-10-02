33e96e5bc99881cb36f10445d8c592c6a094dfaf
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-10-02 13:58:05 +0200

    final HomeRepo homeRepo;

--- lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,1 +1,7 @@
-class FetchFeaturedBooksUseCase {}
+import '../repos/home_repo.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+}
