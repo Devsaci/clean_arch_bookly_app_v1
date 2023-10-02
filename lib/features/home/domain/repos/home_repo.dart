6c8ddd735a2ed0396c05e4db919150726cb6be7c
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-10-02 10:46:06 +0200

    Future<Either<Failure, List<BookEntity>>>

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,6 +1,8 @@
+import 'package:clean_arch_bookly_app_v1/core/errors/failure.dart';
 import 'package:clean_arch_bookly_app_v1/features/home/domain/entities/book_entitiy.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
   Future<List<BookEntity>> fetchNewsetBooks();
 }
