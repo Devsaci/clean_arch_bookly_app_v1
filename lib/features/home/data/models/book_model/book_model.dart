22dac52ec831fa73c69a6db43b1bd766c5a7305a
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-10-04 14:37:18 +0200

    bookId: id!,

--- lib/features/home/data/models/book_model/book_model.dart
+++ lib/features/home/data/models/book_model/book_model.dart
@@ -29,7 +29,7 @@ class BookModel extends BookEntity {
           authorName: '',
           price: null,
           rating: null,
-          bookId: '',
+          bookId: id!,
         );
 
   factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
