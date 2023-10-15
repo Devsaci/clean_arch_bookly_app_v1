e040bd14e1bf50483eb81196c100e668b1418478
saci zakaria <sacideveloppeur@gmail.com>
2023-10-15 12:29:30 +0200

    async

--- lib/core/uttlls/api_service.dart
+++ lib/core/uttlls/api_service.dart
@@ -5,7 +5,7 @@ class ApiService {
   final baseUrl = "https://www.googleapis.com/books/v1/";
 
   ApiService(this._dio);
-  get({required String endPoint}) {
-    _dio.get('$baseUrl$endPoint');
+  get({required String endPoint}) async {
+    await _dio.get('$baseUrl$endPoint');
   }
 }
