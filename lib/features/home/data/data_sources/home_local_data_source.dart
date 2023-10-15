56407929d21f3439669f0574f14a2e013ceff53c
saci zakaria <sacideveloppeur@gmail.com>
2023-10-15 17:11:14 +0200

    List<BookEntity> fetchFeaturedBooks();

--- lib/features/home/data/data_sources/home_local_data_source.dart
+++ lib/features/home/data/data_sources/home_local_data_source.dart
@@ -1,1 +1,13 @@
-abstract class HomeLocalDataSource {}
+import 'package:clean_arch_bookly_app_v1/features/home/domain/entities/book_entitiy.dart';
+
+abstract class HomeLocalDataSource {
+  List<BookEntity> fetchFeaturedBooks();
+}
+
+class HomeLocalDataSourceImpl extends HomeLocalDataSource {
+  @override
+  List<BookEntity> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
