01af8026ef8d0730418c70cbc48a245273431b52
saci zakaria <sacideveloppeur@gmail.com>
2023-10-15 12:32:10 +0200

    Future<Map<String, dynamic>>

--- lib/core/uttlls/api_service.dart
+++ lib/core/uttlls/api_service.dart
@@ -5,7 +5,7 @@ class ApiService {
   final baseUrl = "https://www.googleapis.com/books/v1/";
 
   ApiService(this._dio);
-  get({required String endPoint}) async {
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
     var response = await _dio.get('$baseUrl$endPoint');
     return response.data;
   }
