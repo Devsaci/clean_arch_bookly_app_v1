0cf52d572d8bade81ebe701636a88f2a78c4c38a
saci zakaria <sacideveloppeur@gmail.com>
2023-10-15 13:59:33 +0200

    Future<List<BookEntity>> fetchNewestBooks();

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -2,12 +2,19 @@ import '../../domain/entities/book_entitiy.dart';
 
 abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
 }
 
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
   @override
   Future<List<BookEntity>> fetchFeaturedBooks() {
     // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
     throw UnimplementedError();
   }
 }
