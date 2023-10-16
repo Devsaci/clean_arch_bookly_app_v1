b2c361e45332b0edd9b3e1e9eaf3a2a94174e077
saci zakaria <sacideveloppeur@gmail.com>
2023-10-16 09:35:24 +0200

    Hive.registerAdapter(BookEntityAdapter());

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:clean_arch_bookly_app_v1/core/uttlls/app_router.dart';
+import 'package:clean_arch_bookly_app_v1/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:clean_arch_bookly_app_v1/constants.dart';
 // import 'package:clean_arch_bookly_app_v1/features/home/presentation/views/home_view.dart';
 // import 'package:clean_arch_bookly_app_v1/features/splash/presentation/views/splash_view.dart';
 // import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
 void main() {
   runApp(const Bookly());
+  Hive.registerAdapter(BookEntityAdapter());
 }
 
 class Bookly extends StatelessWidget {
