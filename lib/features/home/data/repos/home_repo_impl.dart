7dfa182acb3a4838bfa2094444513a0568300726
saci zakaria <sacideveloppeur@gmail.com>
2023-10-16 11:26:24 +0200

    final HomeRemoteDataSource / final HomeLocalDataSourceImpl

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,9 +1,16 @@
 import 'package:clean_arch_bookly_app_v1/core/errors/failure.dart';
+import 'package:clean_arch_bookly_app_v1/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:clean_arch_bookly_app_v1/features/home/data/data_sources/home_remote_data_source.dart';
 import 'package:clean_arch_bookly_app_v1/features/home/domain/entities/book_entity.dart';
 import 'package:clean_arch_bookly_app_v1/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSourceImpl homeLocalDataSourceImpl;
+
+  HomeRepoImpl(this.homeRemoteDataSource, this.homeLocalDataSourceImpl);
+
   @override
   Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
     // TODO: implement fetchFeaturedBooks
