52a62818d82d38e68d70892885cdda591e2ab472
saci zakaria <sacideveloppeur@gmail.com>
2023-10-16 10:44:49 +0200

    saveBooksData(books, kNewestBox);

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -32,7 +32,7 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
         endPoint:
             'volumes?=Filtering=free-ebooks&Sorting=newest&q=prohramming');
     List<BookEntity> books = getBooksList(data);
-
+    saveBooksData(books, kNewestBox);
     return books;
   }
 
