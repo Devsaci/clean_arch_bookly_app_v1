8e67621f55f470bc0e459e741ce444175bdb1027
saci zakaria <sacideveloppeur@gmail.com>
2023-10-17 15:40:03 +0200

    return ServerFailure.fromResponse

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -22,13 +22,25 @@ class ServerFailure extends Failure {
       case DioErrorType.badCertificate:
         return ServerFailure('badCertificate with api server');
       case DioErrorType.badResponse:
-      // ToDo
+        return ServerFailure.fromResponse(
+            e.response!.statusCode!, e.response!.data);
       case DioErrorType.cancel:
         return ServerFailure('Request to ApiServer was canceld');
       case DioErrorType.connectionError:
         return ServerFailure('No Internet Connection');
       case DioErrorType.unknown:
         return ServerFailure('Opps There was an Error, Please try again');
+    }
+  }
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 404) {
+      return ServerFailure('Your request was not found, please try later');
+    } else if (statusCode == 500) {
+      return ServerFailure('There is a problem with server, please try later');
+    } else if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else {
+      return ServerFailure('There was an error , please try again');
     }
   }
 }
