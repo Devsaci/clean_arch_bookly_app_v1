cb2ad100f0c2658f2e06f769a49775125f080a6a
saci zakaria <sacideveloppeur@gmail.com>
2023-10-17 15:38:41 +0200

    switch (e.type)

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: deprecated_member_use, duplicate_ignore
+
+import 'package:dio/dio.dart';
+
 abstract class Failure {
   final String message;
 
@@ -6,7 +10,25 @@ abstract class Failure {
 
 class ServerFailure extends Failure {
   ServerFailure(super.message);
-  factory ServerFailure.fromDiorError(message) {
-    return ServerFailure(message);
+
+  factory ServerFailure.fromDiorError(DioError e) {
+    switch (e.type) {
+      case DioErrorType.connectionTimeout:
+        return ServerFailure('Connection timeout with api server');
+      case DioErrorType.sendTimeout:
+        return ServerFailure('Send timeout with ApiServer');
+      case DioErrorType.receiveTimeout:
+        return ServerFailure('Receive timeout with ApiServer');
+      case DioErrorType.badCertificate:
+        return ServerFailure('badCertificate with api server');
+      case DioErrorType.badResponse:
+      // ToDo
+      case DioErrorType.cancel:
+        return ServerFailure('Request to ApiServer was canceld');
+      case DioErrorType.connectionError:
+        return ServerFailure('No Internet Connection');
+      case DioErrorType.unknown:
+        return ServerFailure('Opps There was an Error, Please try again');
+    }
   }
 }
