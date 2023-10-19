fe7de22d8b5f892ff6739adb353ce4c7df053bfa
saci zakaria <sacideveloppeur@gmail.com>
2023-10-19 14:58:56 +0200

    final FetchNewestdBooksUseCase fetchNewestdBooksUseCase;

--- lib/features/home/presentation/manger/newset_books_cubit/newset_books_cubit.dart
+++ lib/features/home/presentation/manger/newset_books_cubit/newset_books_cubit.dart
@@ -2,9 +2,11 @@ import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 import '../../../domain/entities/book_entity.dart';
+import '../../../domain/use_cases/fetch_newest_books_use_case.dart';
 
 part 'newset_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.fetchNewestdBooksUseCase) : super(NewestBooksInitial());
+  final FetchNewestdBooksUseCase fetchNewestdBooksUseCase;
 }
