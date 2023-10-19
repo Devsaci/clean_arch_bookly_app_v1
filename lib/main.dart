4e9aa5484603d0ceff84ffef9a22450bbcd41137
saci zakaria <sacideveloppeur@gmail.com>
2023-10-19 16:03:49 +0200

    FeaturedBooksCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
+import 'package:clean_arch_bookly_app_v1/core/uttlls/api_service.dart';
 import 'package:clean_arch_bookly_app_v1/core/uttlls/app_router.dart';
+import 'package:clean_arch_bookly_app_v1/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:clean_arch_bookly_app_v1/features/home/data/repos/home_repo_impl.dart';
 import 'package:clean_arch_bookly_app_v1/features/home/domain/entities/book_entity.dart';
+import 'package:clean_arch_bookly_app_v1/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:clean_arch_bookly_app_v1/constants.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 // import 'package:clean_arch_bookly_app_v1/features/home/presentation/views/home_view.dart';
 // import 'package:clean_arch_bookly_app_v1/features/splash/presentation/views/splash_view.dart';
 // import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 // import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
+
+import 'features/home/data/data_sources/home_remote_data_source.dart';
+import 'features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 
 void main() async {
   runApp(const Bookly());
@@ -26,11 +35,32 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) {
+            return FeaturedBooksCubit(
+              FetchFeaturedBooksUseCase(
+                HomeRepoImpl(
+                  homeLocalDataSource: HomeLocalDataSourceImpl(),
+                  homeRemoteDataSource: HomeRemoteDataSourceImpl(
+                    ApiService(
+                      Dio(),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
