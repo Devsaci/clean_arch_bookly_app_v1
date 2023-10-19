6e15a563636c21805b8adc4993ab01aefe8596eb
saci zakaria <sacideveloppeur@gmail.com>
2023-10-19 14:31:33 +0200

    final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;

--- lib/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:clean_arch_bookly_app_v1/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:meta/meta.dart';
 
 import '../../../domain/entities/book_entity.dart';
@@ -6,7 +7,9 @@ import '../../../domain/entities/book_entity.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.fetchFeaturedBooksUseCase)
+      : super(FeaturedBooksInitial());
+  final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
 
   Future<void> fetchFeaturedBooks() async {
     emit(FeaturedBooksLoading());
