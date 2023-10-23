b01b01076fe7853f71a01ab614e39c785143f6c7
saci zakaria <sacideveloppeur@gmail.com>
2023-10-23 17:11:16 +0200

    void onChange

--- lib/core/uttlls/simple_bloc_observer.dart
+++ lib/core/uttlls/simple_bloc_observer.dart
@@ -1,33 +1,10 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SimpleBlocObserver implements BlocObserver {
+class SimpleBlocObserver extends BlocObserver {
   @override
   void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+
     // TODO: implement onChange
-  }
-
-  @override
-  void onClose(BlocBase bloc) {
-    // TODO: implement onClose
-  }
-
-  @override
-  void onCreate(BlocBase bloc) {
-    // TODO: implement onCreate
-  }
-
-  @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    // TODO: implement onError
-  }
-
-  @override
-  void onEvent(Bloc bloc, Object? event) {
-    // TODO: implement onEvent
-  }
-
-  @override
-  void onTransition(Bloc bloc, Transition transition) {
-    // TODO: implement onTransition
   }
 }
