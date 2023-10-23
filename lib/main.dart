8e42faac5831ee698ec6cc313f924c5f4b6e2cf6
saci zakaria <sacideveloppeur@gmail.com>
2023-10-23 12:11:37 +0200

    <HomeRepoImpl>

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class Bookly extends StatelessWidget {
                 //     ),
                 //   ),
                 // ),
-                getIt.get(),
+                getIt.get<HomeRepoImpl>(),
               ),
             );
           },
