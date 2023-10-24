8fad1a737b806347b912b1c7b12d320a2dd883c4
saci zakaria <sacideveloppeur@gmail.com>
2023-10-24 12:42:12 +0200

    var box = Hive.box<BookEntity>(kFeaturedBox);

--- lib/core/uttlls/functions/save_books.dart
+++ lib/core/uttlls/functions/save_books.dart
@@ -5,6 +5,6 @@ import '../../../constants.dart';
 import '../../../features/home/domain/entities/book_entity.dart';
 
 void saveBooksData(List<BookEntity> books, String boxName) {
-  var box = Hive.box(kFeaturedBox);
+  var box = Hive.box<BookEntity>(kFeaturedBox);
   box.addAll(books);
 }
