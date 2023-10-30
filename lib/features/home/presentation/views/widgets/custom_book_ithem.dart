b49f226f0ec05305a1cbfdb47fdf481d8243a6e3
saci zakaria <sacideveloppeur@gmail.com>
2023-10-30 14:19:32 +0100

    CustumBookImage({Key? key, required this.image})

--- lib/features/home/presentation/views/widgets/custom_book_ithem.dart
+++ lib/features/home/presentation/views/widgets/custom_book_ithem.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import '../../../../../core/uttlls/assets.dart';
 
 class CustumBookImage extends StatelessWidget {
-  const CustumBookImage({super.key, required this.image});
+  //const CustumBookImage({super.key, required this.image});
+  const CustumBookImage({Key? key, required this.image}) : super(key: key);
   final String image;
   @override
   Widget build(BuildContext context) {
