26b957cb150aca38747ae5303cdd9b497f0338df
saci zakaria <sacideveloppeur@gmail.com>
2023-10-31 19:27:23 +0100

    Future<Either<Failure, List<BookEntity>>> call([int PageNumber = 0])

--- lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -7,13 +7,15 @@ import 'package:dartz/dartz.dart';
 import '../../../../core/use_case/use_case.dart';
 import '../repos/home_repo.dart';
 
-class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, int> {
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase(this.homeRepo);
 
   @override
-  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
-    return await homeRepo.fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> call([int PageNumber = 0]) async {
+    return await homeRepo.fetchFeaturedBooks(
+      pageNumber: PageNumber,
+    );
   }
 }
